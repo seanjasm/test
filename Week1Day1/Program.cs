fb25f8391d601bee775b2bd3882868712355c90e
seanjasm <seanjasm@gmail.com>
2019-04-22 13:56:11 -0400

    First little console app on day 1 of class

---
+++ Week1Day1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfo
+{        
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            Console.WriteLine("What's name?");
+            input = Console.ReadLine();
+            Console.WriteLine("\nHi {0}!\n", input);
+
+            Console.WriteLine("What's your age?");
+            input = Console.ReadLine();
+
+            if (int.TryParse(input, out int result))
+            {
+                Console.WriteLine("\nAt age {0}, I was {1} \n", input, result > 25 ? "a superhero" : "a pirate");
+            }
+            else
+            {                
+                Console.WriteLine("\nYou are an alien! LOL\n");
+            }
+            
+            Console.WriteLine("What's your favorite color?");
+            input = Console.ReadLine();
+            Console.WriteLine("\nWhat a coincident, my favorite color is {0}, too!\n\n", input);
+
+        }
+    }
+}
